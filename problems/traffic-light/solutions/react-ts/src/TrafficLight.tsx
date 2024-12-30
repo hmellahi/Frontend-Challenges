f8603b76b36479fdbe004032b9a12e6088eb5dfb
hmellahi <hamzasenpai307@gmail.com>
2024-12-30 06:41:50 +0100

    improve traffic-light accessibility and update DOCs

--- problems/traffic-light/solutions/react-ts/src/TrafficLight.tsx
+++ problems/traffic-light/solutions/react-ts/src/TrafficLight.tsx
@@ -1,8 +1,9 @@
 import { useEffect, useState } from "react";
 import { Config } from "./types";
+import { cn } from "./utils/cn";
 
 function Light({ backgroundColor }: { backgroundColor: string }) {
-  return <div className="traffic-light" style={{ backgroundColor }}></div>;
+  return <div className="traffic-light" style={{ backgroundColor }} />;
 }
 
 export default function TrafficLight({
@@ -16,23 +17,23 @@ export default function TrafficLight({
 
   useEffect(() => {
     const { next, duration } = config[currentLight];
-    const interval = setTimeout(() => {
+    const timerId = setTimeout(() => {
       setCurrentLight(next);
     }, duration);
 
     return () => {
-      clearInterval(interval);
+      clearTimeout(timerId);
     };
-  }, [currentLight]);
+  }, [currentLight, config]);
 
   return (
     <div
-      className={[
+      role="alert"
+      aria-label={`Traffic light is ${currentLight}`}
+      className={cn([
         "traffic-light-container",
         layout === "vertical" && "traffic-light-container--vertical",
-      ]
-        .filter((cls) => !!cls)
-        .join(" ")}
+      ])}
     >
       {Object.keys(config).map((color) => (
         <Light
