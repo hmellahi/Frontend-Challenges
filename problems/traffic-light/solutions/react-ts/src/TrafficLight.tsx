45f4a11146f5cd2898e2f566d2b009223d3d2f45
hmellahi <hamzasenpai307@gmail.com>
2024-12-30 06:58:55 +0100

    add types to Traffic-light

--- problems/traffic-light/solutions/react-ts/src/TrafficLight.tsx
+++ problems/traffic-light/solutions/react-ts/src/TrafficLight.tsx
@@ -11,7 +11,7 @@ export default function TrafficLight({
   layout = "vertical",
 }: {
   config: Config;
-  layout?: string;
+  layout?: "vertical" | "horizontal";
 }) {
   const [currentLight, setCurrentLight] = useState("green");
 
