077c5fc77a2cc062af89682a5285b394294b9e1c
Mateo Verdaguer <mateoverdaguer123@gmail.com>
2025-01-09 18:41:37 -0300

    fix style

--- problems/traffic-light/solutions/my-solution/src/components/TrafficLight.tsx
+++ problems/traffic-light/solutions/my-solution/src/components/TrafficLight.tsx
@@ -28,13 +28,13 @@ const TrafficLight = () => {
   return (
     <div className="flex flex-col items-center gap-12">
 
-      <div className="bg-black py-6 h-[56vh] w-[16vw] rounded-2xl flex flex-col gap-6 items-center">
+      <div className="bg-black py-6 h-[56vh] w-36 rounded-2xl flex flex-col gap-6 items-center">
         <div className={`${activeColor.color === 'red' ? "bg-red-500" : "bg-gray-500"} h-[15vh] w-[15vh] rounded-full`}></div>
         <div className={`${activeColor.color === 'yellow' ? "bg-yellow-500" : "bg-gray-500"} h-[15vh] w-[15vh] rounded-full`}></div>
         <div className={`${activeColor.color === 'green' ? "bg-green-500" : "bg-gray-500"} h-[15vh] w-[15vh] rounded-full`}></div>
       </div>
 
-      <div className="bg-black py-6 h-[16vw] w-[56vh] rounded-2xl flex justify-center gap-6">
+      <div className="bg-black py-4 h-36 w-[56vh] rounded-2xl flex justify-center gap-6">
         <div className={`${activeColor.color === 'red' ? "bg-red-500" : "bg-gray-500"} h-[15vh] w-[15vh] rounded-full`}></div>
         <div className={`${activeColor.color === 'yellow' ? "bg-yellow-500" : "bg-gray-500"} h-[15vh] w-[15vh] rounded-full`}></div>
         <div className={`${activeColor.color === 'green' ? "bg-green-500" : "bg-gray-500"} h-[15vh] w-[15vh] rounded-full`}></div>
